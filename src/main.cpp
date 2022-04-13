ff10a2c39899c2c860d1c4e4a8b01eebc0c38427
demitriaswilbert <demitriaswilbert@gmail.com>
2022-04-13 09:11:12 +0700

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,26 @@
 
 using namespace std;
 
+/**
+ * @brief Fungsi untuk mengambil data
+ * 
+ * Fungsi ini dibuat untuk mengambil data-data yang berulang pada case 1 s/d 3
+ * seperti nama, npp/nrp, id, tanggal lahir, departemen, dan lainnya, agar tidak terjadi 
+ * kode berulang yang dapat mengurangi keterbacaan kode.
+ * 
+ * @param prompt referensi dari vektor yang mengandung string untuk memberi konteks input
+ * 
+ * seperti "Input nama : ", "Input id : ", dsb.
+ * 
+ * @param pStr referensi dari vektor yang mengandung pointer data input yang berupa string
+ * 
+ * seperti nama, id, nrp, departement, dsb.
+ * 
+ * @param pInt referensi dari vektor yang mengandung pointer data input integer
+ * 
+ * seperti tanggal, bulan, dan tahun lahir, tahun masuk, sks Lulus, semester, dsb.
+ * 
+ */
 void getInput(vector<string>& prompt, vector<string*>& pStr, vector<int*>& pInt)
 {
     int promptCnt = 0;
@@ -24,8 +44,9 @@ int main()
 	vector<tendik> recTendik;
 
 	int menu_terpilih;
-
-	while(1) {
+    bool loopContinue = 1;
+	while(loopContinue) {
+        // display menu
 		cout << "Selamat datang di Universitas X" << endl << endl;
 		cout << "Data statistik:" << endl;
 		cout << "  1. Jumlah Mahasiswa             : " << recMhs.size() << " mahasiswa" << endl;
@@ -39,14 +60,23 @@ int main()
 		cout << "  4. Tampilkan semua Mahasiswa" << endl;
 		cout << "  5. Tampilkan semua Dosen" << endl;
 		cout << "  6. Tampilkan semua Tenaga Kependidikan" << endl;
+        cout << "  7. Keluar" << endl;
 		cout << "-> Silahkan memilih salah satu: ";
+
+        // mengambil pilihan menu user
 		fflush(stdin); cin >> menu_terpilih;
         
+        // switch case sesuai pada pilihan user
 		switch (menu_terpilih) {
-			case 1:
+            
+			case 1: // input mahasiswa baru
             {
+                // deklarasi variabel string dan int
                 string nama, id, nrp, dept; 
                 int dd, mm, yy, tm, sksLulus, semester;
+
+                // definisi vektor string untuk ditampilkan ke terminal
+                // untuk menjadi konteks input data mahasiswa
                 vector<string> prompt = {
                     "Input nama                     : ", "Input id                       : ",
                     "Input nrp                      : ", "Input Departement              : ",
@@ -54,61 +84,104 @@ int main()
                     "Input Tahun Lahir (int)        : ", "Input Tahun masuk (int)        : ", 
                     "Input SKS Lulus (int)          : ", "Input Semester ke (int)        : "
                 };
+
+                // menggabungkan pointer dari masing-masing variabel input kedalam sebuah vektor 
                 vector<string*> pStr = {&nama, &id, &nrp, &dept};
                 vector<int*> pInt = {&dd, &mm, &yy, &tm, &sksLulus, &semester};
-                getInput(prompt, pStr, pInt);
-                mahasiswa tmp = mahasiswa(id, nama, dd, mm, yy, nrp, dept, tm);
-                tmp.setSKSLulus(sksLulus); tmp.setSemester(semester < 1? 1 : semester);
 
-                for(int i = 1; i < semester; i++)
+                // mengambil input
+                getInput(prompt, pStr, pInt);
+
+                // deklarasi objek mahasiswa baru
+                mahasiswa tmp = mahasiswa(id, nama, dd, mm, yy, nrp, dept, tm);
+
+                // set nilai semester dan sks lulus menggunakan fungsi setter dari kelas
+                tmp.setSKSLulus(sksLulus); tmp.setSemester(semester = semester < 1? 1 : semester > 14? 14 : semester);
+
+                // input nilai sks untuk masing-masing semester
+                for(int i = 0; i < semester; i++)
                 {
                     float ipsTmp;
-                    std::cout << "IPS Semester ke " << i << " (int)      : "; fflush(stdin); cin >> ipsTmp;
-                    tmp.setIPS(i, ipsTmp);
+                    std::cout << "IPS Semester ke " << i + 1 << " (int)      : "; fflush(stdin); cin >> ipsTmp;
+                    tmp.setIPS(i+1, ipsTmp);
                 }
+
+                // masukkan objek mahasiswa yang telah dibuat kedalam vektor mahasiswa
                 recMhs.push_back(tmp);
 				break;
             }
-			case 2:
+            
+			case 2: // input dosen baru
             {
+                // deklarasi variabel string dan int
                 string nama, id, npp, dept; 
                 int dd, mm, yy, pd;
+
+                // definisi vektor string untuk ditampilkan ke terminal 
+                // untuk menjadi konteks input data dosen
                 vector<string> prompt = {
                     "Input nama                     : ", "Input id                       : ",
                     "Input npp                      : ", "Input Departement              : ",
                     "Input Tanggal Lahir (angka)    : ", "Input Bulan Lahir (angka)      : ",
                     "Input Tahun Lahir (angka)      : ", "Input Pendidikan (angka)       : ",
                 };
+
+                // menggabungkan pointer dari masing-masing variabel input kedalam sebuah vektor 
                 vector<string*> pStr = {&nama, &id, &npp, &dept};
                 vector<int*> pInt = {&dd, &mm, &yy, &pd};
+
+                // mengambil input
                 getInput(prompt, pStr, pInt);
+
+                // deklarasi objek dosen baru
                 dosen tmp = dosen(id, nama, dd, mm, yy, npp, dept, pd);
+                
+                // masukkan objek dosen yang telah dibuat kedalam vektor dosen
                 recDosen.push_back(tmp);
 				break;
             }
-			case 3:
+            
+			case 3: // input tendik baru
             {
+                // deklarasi variabel string dan int
                 string nama, id, npp, unit; 
                 int dd, mm, yy;
+
+                // definisi vektor string untuk ditampilkan ke terminal 
+                // untuk menjadi konteks input data tendik
                 vector<string> prompt = {
                     "Input nama                     : ", "Input id                       : ",
                     "Input npp                      : ", "Input Unit                     : ",
                     "Input Tanggal Lahir (angka)    : ", "Input Bulan Lahir (angka)      : ",
                     "Input Tahun Lahir (angka)      : ",
                 };
+
+                // menggabungkan pointer dari masing-masing variabel input kedalam sebuah vektor 
                 vector<string*> pStr = {&nama, &id, &npp, &unit};
                 vector<int*> pInt = {&dd, &mm, &yy};
+
+                // mengambil input
                 getInput(prompt, pStr, pInt);
+
+                // deklarasi objek tendik baru
                 tendik tmp = tendik(id, nama, dd, mm, yy, npp, unit);
+
+                // masukkan objek tendik yang telah dibuat kedalam vektor tendik
                 recTendik.push_back(tmp);
 				break;
             }
-			case 4:
+			case 4: // tampilkan data mahasiswa
             {
+                // definisi counter i menjadi 1
                 int i = 1;
+
+                // tampilkan jumlah mahasiswa di dalam sistem
                 cout << "Terdapat " << recMhs.size() << " Mahasiswa di dalam sistem ini." << endl;
+
+                // iterasikan variabel a dari setiap objek di recMhs
                 for(auto a : recMhs)
                 {
+                    // tampilkan data tiap mahasiswa
                     cout << "\n" << i++ << ".\n";
                     cout << "    Nama           : " << a.getNama() << endl;
                     cout << "    NRP            : " << a.getNRP() << endl;
@@ -122,20 +195,27 @@ int main()
                     cout << "    IPK            : " << a.getIPK() << endl;
                     cout << "    IPS            : { " ;
 
-                    for(int j=1, comma=0; j < a.getSemester(); j++, comma=1)
-                        cout << ((comma)? ", " : "") << a.getIPS(j);
+                    // for loop untuk menampilkan nilai sks dalam bentuk array C
+                    for(int j=0, comma=0; j < a.getSemester(); j++, comma=1)
+                        cout << ((comma)? ", " : "") << a.getIPS(j+1);
                     cout << " }" << endl;
 
                 }
                 
 				break;
             }
-			case 5:
+			case 5: // tampilkan data dosen
             {
+                // definisi counter i menjadi 1
                 int i = 1;
+
+                // tampilkan jumlah dosen di dalam sistem
                 cout << "Terdapat " << recDosen.size() << " Dosen di dalam sistem ini." << endl;
+
+                // iterasikan variabel a pada setiap objek di recDosen
                 for(auto a : recDosen)
                 {
+                    // tampilkan data dosen
                     cout << "\n" << i++ << ".\n";
                     cout << "    Nama           : " << a.getNama() << endl;
                     cout << "    NPP            : " << a.getNPP() << endl;
@@ -146,12 +226,18 @@ int main()
                 }
 				break;
             }
-			case 6:
+			case 6: // tampilkan data tendik
 			{
+                // definisi counter i menjadi 1
                 int i = 1;
+                
+                // tampilkan jumlah dosen di dalam sistem
                 cout << "Terdapat " << recTendik.size() << " Tendik di dalam sistem ini." << endl;
+
+                // iterasikan variabel a pada setiap objek di recDosen
                 for(auto a : recTendik)
                 {
+                    // tampilkan data dosen
                     cout << "\n" << i++ << ".\n";
                     cout << "    Nama           : " << a.getNama() << endl;
                     cout << "    NPP            : " << a.getNPP() << endl;
@@ -161,6 +247,10 @@ int main()
                 }
 				break;
             }
+            case 7: // keluar dari program
+                cout << "Berhasil Keluar dari Program";
+                loopContinue = 0;
+                break;
 		}
 	}
 
