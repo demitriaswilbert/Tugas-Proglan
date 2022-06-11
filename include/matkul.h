dfb51b931f68306533e0d31bab3ae5b3dec044c3
demitriaswilbert <demitriaswilbert@gmail.com>
2022-06-11 16:19:03 +0700

    add matkul.h

---
+++ include/matkul.h
@@ -0,0 +1,13 @@
+#ifndef MATKUL_HPP
+#define MATKUL_HPP
+
+#include <bits/stdc++.h>
+#include "include/kelas.h"
+
+class Matkul {
+    private:
+    std::string name;
+    std::vector<Kelas> recKelas; 
+};
+
+#endif
